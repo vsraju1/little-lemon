dbc966ec8d99bf928178901c55329818b32f4b54
V SHANMUKHA RAJU <rajunarayana1248@gmail.com>
2024-07-03 12:39:52 +0530

    added meta tags

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,43 @@
 import { useState } from 'react'
 import './App.css'
 
+const Header = () => {
+  return (
+    <>
+    <header>
+      <nav>
+        <ul>
+          <li>Nav Logo</li>
+        </ul>
+      </nav>
+    </header>
+    </>
+  )
+}
+
+const Main = () => {
+  return (
+    <>
+    <main>This is main element</main>
+    </>
+  )
+}
+
+const Footer = () => {
+  return (
+    <>
+    <footer>This is footer element</footer>
+    </>
+  )
+}
+
+
 function App() {
   return (
     <>
-      <div>This is vite app</div>
+      <Header />
+      <Main />
+      <Footer />
     </>
   )
 }
